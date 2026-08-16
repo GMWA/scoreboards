8bcd7c18ccc3b4373339d169d0dd7e7e1c96e70a
meli.tsaki.susy <susymeli25@gmail.com>
2026-08-16 01:50:15 +0100

    fix: migrate remaining player endpoints to fetchPaginated (#8)

--- lib/services/player.dart
+++ lib/services/player.dart
@@ -3,22 +3,18 @@ import 'package:http/http.dart';
 import 'package:scoreboards/models/player.dart';
 import 'package:scoreboards/models/transfert.dart';
 import 'package:scoreboards/constants/urls.dart';
+import 'package:scoreboards/services/api_pagination.dart';
 
 class PlayerService {
   static Client client = Client();
   static Future<List<Player>> getPlayersByTeam(int teamId) async {
-    Response res = await client.get(Uri.parse(
-        urls['PLAYERS']['BY_TEAM'].replaceAll('#teamId', teamId.toString())));
-
-    if (res.statusCode == 200) {
-      List<dynamic> playersList = jsonDecode(res.body);
-      List<Player> players =
-          playersList.map((dynamic item) => Player.fromJson(item)).toList();
-      return players;
-    } else {
-      throw Exception('Failed to load players');
-    }
-  }
+  return fetchPaginated(
+    client: client,
+    uri: Uri.parse(
+        urls['PLAYERS']['BY_TEAM'].replaceAll('#teamId', teamId.toString())),
+    fromJson: (item) => Player.fromJson(item),
+  );
+}
 
   static Future<Player> getPlayerBySlug(String slug) async {
     Response res =
@@ -34,119 +30,63 @@ class PlayerService {
   }
 
   static Future<List<PlayerStats>> getPlayerStatsByEdition(
-      int editionId) async {
-    try {
-      final res = await client.get(Uri.parse(
-        urls['PLAYERS']['STATS_BY_EDITION']
-            .replaceAll('#editionId', editionId.toString()),
-      ));
+    int editionId) async {
+  try {
+    final res = await client.get(Uri.parse(
+      urls['PLAYERS']['STATS_BY_EDITION']
+          .replaceAll('#editionId', editionId.toString()),
+    ));
 
-      if (res.statusCode == 200) {
-        final playersList = jsonDecode(res.body);
+    if (res.statusCode == 200) {
+      final playersList = jsonDecode(res.body);
 
-        final playerStats = (playersList as List)
-            .map((item) => PlayerStats.fromJson(item))
-            .toList();
+      final playerStats = (playersList as List)
+          .map((item) => PlayerStats.fromJson(item))
+          .toList();
 
-        return playerStats;
-      } else {
-        throw Exception("Can't get players stats by edition.");
-      }
-    } catch (e) {
-      rethrow;
+      return playerStats;
+    } else {
+      throw Exception("Can't get players stats by edition.");
     }
+  } catch (e) {
+    rethrow;
   }
+}
 
   static Future<List<PlayerStats>> getPlayerStatsByTeam(int teamId) async {
-    try {
-      final res = await client.get(Uri.parse(
-        urls['PLAYERS']['STATS_BY_TEAM']
-            .replaceAll('#teamId', teamId.toString()),
-      ));
-
-      if (res.statusCode == 200) {
-        final playersList = jsonDecode(res.body);
-
-        final playerStats = (playersList as List)
-            .map((item) => PlayerStats.fromJson(item))
-            .toList();
-
-        return playerStats;
-      } else {
-        throw Exception("Can't get players stats by team.");
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
+  return fetchPaginated(
+    client: client,
+    uri: Uri.parse(urls['PLAYERS']['STATS_BY_TEAM']
+        .replaceAll('#teamId', teamId.toString())),
+    fromJson: (item) => PlayerStats.fromJson(item),
+  );
+}
 
   static Future<List<PlayerContract>> getPlayerContracts(int playerId) async {
-    try {
-      final res = await client.get(Uri.parse(
-        urls['PLAYERS']['CONTRACT_BY_PLAYER']
-            .replaceAll('#playerId', playerId.toString()),
-      ));
-
-      if (res.statusCode == 200) {
-        final dataList = jsonDecode(res.body);
-
-        final playerContrats = (dataList as List)
-            .map((item) => PlayerContract.fromJson(item))
-            .toList();
-
-        return playerContrats;
-      } else {
-        throw Exception("Can't get players Contacts by player.");
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
+  return fetchPaginated(
+    client: client,
+    uri: Uri.parse(urls['PLAYERS']['CONTRACT_BY_PLAYER']
+        .replaceAll('#playerId', playerId.toString())),
+    fromJson: (item) => PlayerContract.fromJson(item),
+  );
+}
 
   static Future<List<PlayerTransfert>> getPlayerTransfers(int playerId) async {
-    try {
-      final res = await client.get(Uri.parse(
-        urls['PLAYERS']['TRANSFER_BY_PLAYER']
-            .replaceAll('#playerId', playerId.toString()),
-      ));
-
-      if (res.statusCode == 200) {
-        final playersTranseferList = jsonDecode(res.body);
-
-        final playerTransferts = (playersTranseferList as List)
-            .map((item) => PlayerTransfert.fromJson(item))
-            .toList();
-
-        return playerTransferts;
-      } else {
-        throw Exception("Can't get players transfer by player.");
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
+  return fetchPaginated(
+    client: client,
+    uri: Uri.parse(urls['PLAYERS']['TRANSFER_BY_PLAYER']
+        .replaceAll('#playerId', playerId.toString())),
+    fromJson: (item) => PlayerTransfert.fromJson(item),
+  );
+}
 
   static Future<List<PlayerTeamHistory>> getPlayerTeamsHistory(
-      int playerId) async {
-    try {
-      final res = await client.get(Uri.parse(
-        urls['PLAYERS']['PLAYER_TEAM_HISTORY']
-            .replaceAll('#playerId', playerId.toString()),
-      ));
-
-      if (res.statusCode == 200) {
-        final dataList = jsonDecode(res.body);
-
-        final playerTeams = (dataList as List)
-            .map((item) => PlayerTeamHistory.fromJson(item))
-            .toList();
-
-        return playerTeams;
-      } else {
-        throw Exception("Can't get players team history by player.");
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
+    int playerId) async {
+  return fetchPaginated(
+    client: client,
+    uri: Uri.parse(urls['PLAYERS']['PLAYER_TEAM_HISTORY']
+        .replaceAll('#playerId', playerId.toString())),
+    fromJson: (item) => PlayerTeamHistory.fromJson(item),
+  );
+}
 }
